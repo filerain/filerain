369c1342fac707aebb05e00cfffba2f2a56bee57
Andrey Tsarev <andrey.tsarev@fpsoftware.io>
2025-01-31 01:49:17 +0200

    Initial setup

---
+++ go.mod
@@ -0,0 +1,5 @@
+module filerain
+
+go 1.23.4
+
+require github.com/a-h/templ v0.3.819 // indirect
---
+++ main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"embed"
+	"fmt"
+	"github.com/a-h/templ"
+	"io/fs"
+	"net/http"
+)
+
+//go:embed static
+var staticFiles embed.FS
+
+func headers(w http.ResponseWriter, req *http.Request) {
+
+	for name, headers := range req.Header {
+		for _, h := range headers {
+			fmt.Fprintf(w, "%v: %v\n", name, h)
+		}
+	}
+}
+
+func main() {
+	var staticFS = fs.FS(staticFiles)
+	fs := http.FileServer(http.FS(staticFS))
+
+	// Serve static files
+	http.Handle("/", fs)
+
+	http.Handle("/hello", templ.Handler(hello("Title")))
+	http.HandleFunc("/headers", headers)
+
+	http.ListenAndServe(":80", nil)
+}
